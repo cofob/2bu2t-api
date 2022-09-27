912e0ed04bb423b5f64c338373699759c0dc21a6
Egor Ternovoy <cofob@riseup.net>
2022-09-27 21:44:47 +0700

    Add more exception handlers

--- app/exceptions.py
+++ app/exceptions.py
@@ -3,9 +3,11 @@
 from abc import ABCMeta
 
 from fastapi import Request, status
+from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from loguru import logger
 from pydantic import BaseModel
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from .app import app
 
@@ -29,10 +31,12 @@ class AbstractException(Exception, metaclass=ABCMeta):
         self,
         status_code: int = status.HTTP_500_INTERNAL_SERVER_ERROR,
         detail: str | None = None,
+        headers: dict[str, str] | None = None,
     ) -> None:
         """Init method."""
         self.detail = detail
         self.status_code = status_code
+        self.headers = headers
 
         logger.error(self.detail)
 
@@ -50,7 +54,7 @@ class InvalidCIDException(IPFSException):
 
 
 @app.exception_handler(AbstractException)
-async def global_exception_handler(request: Request, exc: AbstractException) -> JSONResponse:
+async def abstract_exception_handler(request: Request, exc: AbstractException) -> JSONResponse:
     """Exception handler for AbstractException.
 
     Returns:
@@ -62,5 +66,41 @@ async def global_exception_handler(request: Request, exc: AbstractException) -> 
             error_code=exc.__class__.__name__,
             detail=exc.detail,
             status_code=exc.status_code,
+        ).dict(),
+        headers=exc.headers,
+    )
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request: Request, exc: StarletteHTTPException) -> JSONResponse:
+    """Exception handler for StarletteHTTPException.
+
+    Returns:
+        JSON serialized ErrorModel.
+    """
+    return JSONResponse(
+        status_code=exc.status_code,
+        content=ErrorModel(
+            error_code="Exception",
+            detail=exc.detail,
+            status_code=exc.status_code,
+        ).dict(),
+        headers=exc.headers,
+    )
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError) -> JSONResponse:
+    """Exception handler for RequestValidationError.
+
+    Returns:
+        JSON serialized ErrorModel.
+    """
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=ErrorModel(
+            error_code="RequestValidationException",
+            detail="Invalid request",
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         ).dict(),
     )
